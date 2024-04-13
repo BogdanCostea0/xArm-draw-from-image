aa30e071ef568f17228eeb8e5f27dc084b6254f5
Bogdan Costea <84265918+BogdanCostea0@users.noreply.github.com>
2024-04-13 20:41:42 +0300

    update 20:41

--- test-run2.py
+++ test-run2.py
@@ -11,25 +11,25 @@ grey = rgb2gray(im)
 coordinates_np = np.argwhere(grey < 0.99)
 coordinates = coordinates_np.tolist()
 
-MAGIC_NUMBER = 0.3
-OFFSET = 50
+MAGIC_NUMBER = 0.25
+OFFSET = 75 
+ZSAFETY = 20
 
 
-sorted_coordinates = pd.DataFrame({'x': coordinates_np[:, 1], 'y': coordinates_np[:, 0], 'z': 60})
+sorted_coordinates = pd.DataFrame({'x': coordinates_np[:, 1], 'y': coordinates_np[:, 0], 'z': ZSAFETY})
 sorted_coordinates.sort_values('x', inplace=True)
 
 robot_trajectory = sorted_coordinates[['x', 'y', 'z']].values.tolist()
-
 for each_pair_of_coord in robot_trajectory:
 
     each_pair_of_coord[0]= int(each_pair_of_coord[0] * MAGIC_NUMBER + OFFSET)
-    each_pair_of_coord[1]= int(each_pair_of_coord[1] * MAGIC_NUMBER + OFFSET)
+    each_pair_of_coord[1]= int(each_pair_of_coord[1] * MAGIC_NUMBER )
     print(f'X: {each_pair_of_coord[0]} | Y: {each_pair_of_coord[1]} | Z: {each_pair_of_coord[2]} ')
 
 counter = 0
 robot_trajectory2 = []
 for each_pair_of_coord in robot_trajectory:
-    if counter % 25 == 0:
+    if counter % 100 == 0:
         robot_trajectory2.append(each_pair_of_coord)
     counter += 1
 
@@ -76,13 +76,11 @@ arm.set_mode(0)
 arm.set_state(state=0)
 arm.reset(wait=True)
 
-speed = 50
+speed = 100
 
 for eachPairOfCoords in robot_trajectory:
-    print(f'Go to X:{eachPairOfCoords[0]}  Y:{eachPairOfCoords[1]}  Z:{eachPairOfCoords[2]}')
-    arm.set_position(x=eachPairOfCoords[0], y=eachPairOfCoords[1], z=eachPairOfCoords[2], wait=True)
-    # print(arm.get_servo_angle(), arm.get_servo_angle(is_radian=False))
-    # time.sleep(0.05)
+        print(f'Go to X:{eachPairOfCoords[0]}  Y:{eachPairOfCoords[1]}  Z:{eachPairOfCoords[2]}')
+        arm.set_position(x=eachPairOfCoords[0], y=eachPairOfCoords[1], z=eachPairOfCoords[2], wait=True)
 
 arm.reset(wait=True)
 
