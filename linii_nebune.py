27fd97272b999d2e24df67065b338b2e9426c707
ZuZu-Mihail <mihailhanga@gmail.com>
2024-04-14 01:46:53 +0300

    a 8a

--- linii_nebune.py
+++ linii_nebune.py
@@ -4,44 +4,51 @@ import matplotlib.pyplot as plt
 import cv2
 
 
-image = cv2.imread("./test_files/daria.jpeg")
+image = cv2.imread("../test_files/test3.png")
 
 # convert to grayscale
 grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # perform edge detection
-edges = cv2.Canny(grayscale, 30, 100)
+edges = cv2.Canny(grayscale, 50, 100)
 
 # detect lines in the image using hough lines technique
-lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, np.array([]), 80, 40)
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, np.array([]), 20, 10)
 
-s_a_gasit = True
-contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-try:
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            cv2.line(image, (x1, y1), (x2, y2), (20, 220, 20), 3)
-except TypeError:
+if lines is None or len(lines) < 5:
+    print('No lines found')
 
     # detect lines in the image using contour technique
-    s_a_gasit = False
-
-    # for contour in contours:
-        # cv2.drawContours(image, [contour], -1, (20, 220, 20), 3)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # make sure that the contours are not empty
 
     # transform the contours into lines for better visualization
     lines = []
-
     for contour in contours:
         for i in range(len(contour) - 1):
             x1, y1 = contour[i][0]
             x2, y2 = contour[i + 1][0]
-            # cv2.line(image, (x1, y1), (x2, y2), (20, 220, 20), 3)
             lines.append([[x1, y1, x2, y2]])
 
     # reduce the number of lines by tolerance (distance between points)
     lines = np.array(lines)
+    tolerance = 18
+    new_lines = []
+
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        if len(new_lines) == 0:
+            new_lines.append(line)
+        else:
+            x1_last, y1_last, x2_last, y2_last = new_lines[-1][0]
+            if np.sqrt((x1 - x1_last) ** 2 + (y1 - y1_last) ** 2) < tolerance:
+                new_lines[-1][0] = [x1_last, y1_last, x2, y2]
+            else:
+                new_lines.append(line)
+    new_lines = np.array(new_lines)
+    lines = new_lines
+
+    # merge lines that are close to each other
     tolerance = 10
     new_lines = []
     for line in lines:
@@ -57,32 +64,29 @@ except TypeError:
     new_lines = np.array(new_lines)
     lines = new_lines
 
-    # reducing the number of lines by tolerance (angle between lines)
+    # make long lines shorter by removing the middle part
+    tolerance = 10
     new_lines = []
     for line in lines:
         x1, y1, x2, y2 = line[0]
-        if len(new_lines) == 0:
+        if np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) > tolerance:
             new_lines.append(line)
-        else:
-            x1_last, y1_last, x2_last, y2_last = new_lines[-1][0]
-            if np.abs(np.arctan2(y2 - y1, x2 - x1) - np.arctan2(y2_last - y1_last, x2_last - x1_last)) < 0.1:
-                new_lines[-1][0] = [x1_last, y1_last, x2, y2]
-            else:
-                new_lines.append(line)
     new_lines = np.array(new_lines)
     lines = new_lines
 
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(image, (x1, y1), (x2, y2), (20, 220, 20), 3)
+    print(f'Number of lines: {len(lines)}')
 
+else:
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(image, (x1, y1), (x2, y2), (20, 220, 20), 3)
 
-print(f'Total number of lines: {len(lines)}' )
-
-# rebuild the image with the new lines
-for line in lines:
-    for x1, y1, x2, y2 in line:
-        cv2.line(image, (x1, y1), (x2, y2), (20, 220, 120), 3)
+    print(f'Number of lines: {len(lines)}')
 
 plt.imshow(image)
-
 plt.show()
 
 # =================================[ROBOT MOVEMENT]==========================
