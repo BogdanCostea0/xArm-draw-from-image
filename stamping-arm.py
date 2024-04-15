af6955d2730cfa67b272ef99e634b32908e24552
Bogdan Costea <84265918+BogdanCostea0@users.noreply.github.com>
2024-04-15 20:59:30 +0300

    Update stamping-arm.py

--- stamping-arm.py
+++ stamping-arm.py
@@ -1,29 +1,72 @@
-# =================================[UI]==============================
+'''
 from tkinter import *
 
+
+def fiir_stamp():
+    choice = 1
+    return choice
+
+def rsp_stamp():
+    choice = 2
+    return choice 
+
+def spd_stamp():
+    choice = 3
+    return choice
+
+def fiirbots_stamp():
+    choice = 4
+    return choice
+
+    # =================================[UI]==============================
+    
+
+GRAY = "#d3d3d3"
+GREEN = "#9bdeac"
+BLUE = "#154c79"
+FONT_NAME = "Courier"
+
+
 window = Tk()
+window.title("Get yout stamp - FIIRBots")
+window.config(padx=40,pady=40,bg=GRAY, highlightthickness=0)
 
-window.title("Get your stamp - FIIRBots")
+canvas = Canvas(width=400, height=400,bg=GRAY)
 
-window.minsize(width=500, height=500)
+my_label = Label(text="Get your stamp - FIIRBOTS", font=("Arial", 24), background=GRAY) 
+my_label.grid(column=1  , row=1)
 
+# creare buton de fiir
+fiir = Button(text="FIIR STAMP", command=fiir_stamp)
+fiir.config(width=15)
+fiir.grid(column=0,row=3)
 
-my_label = Label(text="Te rog alege o varianta", font=("Arial", 24)) 
-my_label.grid(column=0  , row=0)
+# creare buton de rsp
+rsp = Button(text="RSP STAMP", command=rsp_stamp)
+rsp.config(width=15)
+rsp.grid(column=1,row=3)
 
+# creare buton de spd
+spd = Button(text="SPD STAMP", command=spd_stamp)
+spd.config(width=15)
+spd.grid(column=2,row=3)
+
+# creare buton de fiirbots
+FIIRBots = Button(text="FIIRBots STAMP", command=fiirbots_stamp)
+FIIRBots.config(width=15)
+FIIRBots.grid(column=1,row=5)
 
 window.mainloop()
+'''
+
 # =================================[ROBOT MOVEMENT]==========================
 
 import os
 import sys
+choice = input("Alege varianta. 1- FIIRBots, 2 - RSP, 3 - SPD, 4 - FIIRBots")
 
 UP_HEIGHT = 100 # drawing level
 GRAB_HEIGHT = 20 # used to retreat after drawing a line
-
-
-
-
 
 OFFSET = 40
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
@@ -57,58 +100,38 @@ arm.reset(wait=True)
 
 speed = 75
 
+
+
+if int(choice) == 1:
+    # go to grab stamp 
+    yStamp = 0
+elif int(choice) == 2:
+    yStamp = 40
+elif int(choice) == 3:
+    yStamp = 80
+elif int(choice) == 4:
+    yStamp = -40
+    
 # go to grab stamp 
-arm.set_position(x=150, y=0, z=UP_HEIGHT, wait=True,speed=150)
-arm.set_position(x=150, y=0, z=GRAB_HEIGHT, wait=True, speed=50)
-arm.set_position(x=150, y=0, z=UP_HEIGHT, wait=True,speed=150)
+arm.set_position(x=100, y=yStamp, z=UP_HEIGHT, wait=True,speed=150)
+arm.set_position(x=100, y=yStamp, z=GRAB_HEIGHT, wait=True, speed=50)
+arm.set_position(x=100, y=yStamp, z=UP_HEIGHT, wait=True,speed=150)
 
 # go to ink
 arm.set_position(x=250, y=0, z=UP_HEIGHT, wait=True,speed=150)
 arm.set_position(x=250, y=0, z=GRAB_HEIGHT, wait=True,speed=50)
 arm.set_position(x=250, y=0, z=UP_HEIGHT, wait=True,speed=150)
 
-
 # go to stamp flyer
 arm.set_position(x=350, y=0, z=UP_HEIGHT, wait=True,speed=150)
 arm.set_position(x=350, y=0, z=GRAB_HEIGHT, wait=True,speed=50)
 arm.set_position(x=350, y=0, z=UP_HEIGHT, wait=True,speed=150)
 
-
 # go to leave stamp 
-arm.set_position(x=150, y=0, z=UP_HEIGHT, wait=True,speed=150)
-arm.set_position(x=150, y=0, z=GRAB_HEIGHT, wait=True, speed=50)
-arm.set_position(x=150, y=0, z=UP_HEIGHT, wait=True,speed=150)
-# ------
-# arm.set_position(x=150, y=0, z=UP_HEIGHT, wait=True,speed=150)
-# arm.set_position(x=250, y=0, z=GRAB_HEIGHT, wait=True,speed=50)
+arm.set_position(x=150, y=yStamp, z=UP_HEIGHT, wait=True,speed=150)
+arm.set_position(x=150, y=yStamp, z=GRAB_HEIGHT, wait=True, speed=50)
+arm.set_position(x=150, y=yStamp, z=UP_HEIGHT, wait=True,speed=150)
 
 
-
-'''
-#---------------
-
-# arm.set_gripper_position(300, wait=True)
-
-arm.set_position(x=200, y=100, z=UP_HEIGHT, wait=True,speed=150)
-
-arm.set_position(x=250, y=100, z=UP_HEIGHT, wait=True, speed=150)
-
-# --------------
-
-arm.set_position(x=250, y=100, z=GRAB_HEIGHT, wait=True, speed=150)
-
-arm.set_position(x=300, y=100, z=UP_HEIGHT, wait=True, speed=150)
-
-# ---------------
-
-arm.set_position(x=100, y=100, z=UP_HEIGHT, wait=True, speed=150)
-
-arm.set_position(x=100, y=100, z=GRAB_HEIGHT, wait=True, speed=150)
-
-# arm.set_gripper_position(600, wait=True)
-
-'''
-
-arm.reset(wait=True)
-
+arm.reset()
 arm.disconnect()
